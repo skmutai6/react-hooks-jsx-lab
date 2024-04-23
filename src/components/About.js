9d65fbe68490df7b5016dd60aa01a92cd8b75933
skmutai6 <skmutai6@gmail.com>
2024-04-23 19:51:16 +0300

    complete deliverables

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>Hey there, we rock and roll over here!</p>
+    <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+  </div>);
 }
 
 export default About;
